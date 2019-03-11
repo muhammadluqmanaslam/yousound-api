5a968df5954c36b0b6e6b5452322ed049668d33f
Muhammad Luqman Aslam <muhammadluqmanaslam1@gmail.com>
2019-03-11 21:14:15 +0800

    small fix

--- lib/util/number.rb
+++ lib/util/number.rb
@@ -1,18 +1,20 @@
 require 'base_x'
 
 class Util::Number
-  def encode val
-    str = BaseX::RFC4648Base32.integer_to_string(val)
-    if str.length < 5
-      # str = %w(0 1 8 9 0 1 8 9 0 1 8 9).shuffle.sample(5 - str.length).join() + str
-      # str = '9' * (5 - str.length) + str
-      str = '10891'.slice(0, 5 - str.length) + str
+  class << self
+    def encode(val)
+      str = BaseX::RFC4648Base32.integer_to_string(val)
+      if str.length < 5
+        # str = %w(0 1 8 9 0 1 8 9 0 1 8 9).shuffle.sample(5 - str.length).join() + str
+        # str = '9' * (5 - str.length) + str
+        str = '10891'.slice(0, 5 - str.length) + str
+      end
+      str
     end
-    str
-  end
 
-  def decode str
-    str.gsub!(/[0189]/, '')
-    BaseX::RFC4648Base32.string_to_integer(str)
+    def decode(str)
+      str.gsub!(/[0189]/, '')
+      BaseX::RFC4648Base32.string_to_integer(str)
+    end
   end
 end
