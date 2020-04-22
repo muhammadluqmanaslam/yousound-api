9f286b1930cc6fcb8902095c6a35b4f7e00eea48
Muhammad Luqman Aslam <muhammadluqmanaslam1@gmail.com>
2020-04-22 09:51:59 +0800

    show own stories

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -15,11 +15,11 @@ module Api::V1
       per_page = (params[:per_page] || 10).to_i
 
       posts = Post.joins("LEFT JOIN follows ON posts.user_id = follows.followable_id")
-        .where("(follows.blocked = ? AND follows.follower_id = ? AND posts.user_id NOT IN (?)) OR posts.user_id = ?",
+        .where("(follows.blocked = ? AND follows.follower_id = ? AND posts.user_id NOT IN (?)) OR posts.user_id IN (?)",
           false,
           current_user.id,
           current_user.block_list,
-          User.public_relations_user&.id
+          [current_user.id, User.public_relations_user&.id]
         )
         .order('posts.created_at desc').distinct.page(page).per(per_page)
 
