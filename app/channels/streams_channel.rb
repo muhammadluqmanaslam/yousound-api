9b5002cbd6fb9ee8586feb2534cb415c959f10ce
Muhammad Luqman Aslam <muhammadluqmanaslam1@gmail.com>
2020-09-13 21:33:23 -0400

    broadcast stream channel info

--- app/channels/streams_channel.rb
+++ app/channels/streams_channel.rb
@@ -1,8 +1,25 @@
 class StreamsChannel < ApplicationCable::Channel
   def subscribed
     stream_from "stream_#{params[:stream_id]}"
+
+    send_channel_info params[:stream_id]
   end
 
   def unsubscribed
+    send_channel_info params[:stream_id]
+  end
+
+  def send_channel_info(stream_id)
+    channel_name = "stream_#{stream_id}"
+    page_track = "Stream: #{stream_id}"
+    active_viewers_size = ActionCable.server.pubsub.send(:redis_connection).pubsub('numsub', channel_name).dig(1) || 0
+    total_viewers_size = Activity.where('sender_id = receiver_id').where(
+      page_track: page_track,
+      action_type: Activity.action_types[:view_stream]
+    ).size
+    ActionCable.server.broadcast(channel_name, {
+      active_viewers_size: active_viewers_size,
+      total_viewers_size: total_viewers_size
+    })
   end
 end
