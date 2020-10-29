f825b2907fabdfb9fb5146b862083b4917171436
Muhammad Luqman Aslam <muhammadluqmanaslam1@gmail.com>
2020-10-28 21:21:52 -0400

    update token expiration 1 month

--- app/models/json_web_token.rb
+++ app/models/json_web_token.rb
@@ -1,7 +1,9 @@
 class JsonWebToken
   def self.encode(payload, remember_me = false)
-    expiration = 24.hours.from_now.to_i
-    expiration = 15.days.from_now.to_i if remember_me
+    # expiration = 24.hours.from_now.to_i
+    # expiration = 15.days.from_now.to_i if remember_me
+    expiration = 1.months.from_now.to_i
+    expiration = 3.months.from_now.to_i if remember_me
     JWT.encode payload.merge(exp: expiration), Rails.application.secrets.secret_key_base
   end
 
