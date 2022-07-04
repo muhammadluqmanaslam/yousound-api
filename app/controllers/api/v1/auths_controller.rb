c139fb68e203f046f9991e60353f44c34daca3ff
Muhammad Luqman Aslam <muhammadluqmanaslam1@gmail.com>
2022-07-04 17:55:12 +0500

    fix for artist signup

--- app/controllers/api/v1/auths_controller.rb
+++ app/controllers/api/v1/auths_controller.rb
@@ -137,7 +137,9 @@ module Api::V1
         user.attributes = permitted_attributes(user)
         validate_need = true
       end
-
+      if params[:request_role].present?
+        user.user_type = params[:request_role]
+      end
       unless user.request_role.blank?
         user.request_status = User.request_statuses[:pending]
       end
