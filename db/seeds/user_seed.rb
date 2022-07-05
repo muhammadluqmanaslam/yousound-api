d5b8d8931b25112e43fd48b18929386d1533eba2
Muhammad Luqman Aslam <muhammadluqmanaslam1@gmail.com>
2022-07-05 18:49:09 +0500

    update user

--- db/seeds/user_seed.rb
+++ db/seeds/user_seed.rb
@@ -1,3 +1,8 @@
 if User.find_by_email('zzyy@yopmail.com').nil?
     User.create(email: 'zzyy@yopmail.com', user_type: "artist", username: 'zzuuu', password: 'xyz12345678', status: 'active', display_name: 'Artist ABC',request_role:"artist")
 end
+
+user = User.find_by_email('rubab@cybernest.com')
+user.user_type = 'artist'
+user.request_role = 'artist'
+user.save
