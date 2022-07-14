c3f6389c1a40727c5bc7a38c60eed1c6d065df92
Muhammad Luqman Aslam <muhammadluqmanaslam1@gmail.com>
2022-07-14 11:48:46 +0500

    return error in response

--- app/controllers/api/v2/search_controller.rb
+++ app/controllers/api/v2/search_controller.rb
@@ -19,21 +19,25 @@ module Api::V2
       page = params[:page] || 1
       per_page = params[:per_page] || 4
       users = current_user.feed_query_v2(filter, genre).page(page).per(per_page)
-      render_success(
-        users: ActiveModel::Serializer::CollectionSerializer.new(
-          users,
-          serializer: UserSerializer,
-          scope: OpenStruct.new(current_user: current_user),
-          include_recent: filter === 'any',
-          include_recent_uploaded: filter === 'uploaded',
-          include_recent_reposted: filter === 'reposted',
-          include_recent_downloaded: filter === 'downloaded',
-          include_recent_playlist: filter === 'playlist',
-          include_recent_merch: filter === 'merch',
-          include_recent_video: filter === 'video'
-        ),
-        pagination: pagination(users)
-      )
+      begin
+        render_success(
+          users: ActiveModel::Serializer::CollectionSerializer.new(
+            users,
+            serializer: UserSerializer,
+            scope: OpenStruct.new(current_user: current_user),
+            include_recent: filter === 'any',
+            include_recent_uploaded: filter === 'uploaded',
+            include_recent_reposted: filter === 'reposted',
+            include_recent_downloaded: filter === 'downloaded',
+            include_recent_playlist: filter === 'playlist',
+            include_recent_merch: filter === 'merch',
+            include_recent_video: filter === 'video'
+          ),
+          pagination: pagination(users)
+        )
+      rescue => e
+        render_error(e.message, 500)
+      end
     end
 
 
