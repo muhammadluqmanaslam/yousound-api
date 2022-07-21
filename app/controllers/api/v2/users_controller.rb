86dd10252199a29ca66e5b9527fba6a6da8cd5e2
Muhammad Luqman Aslam <muhammadluqmanaslam1@gmail.com>
2022-07-21 19:36:08 +0500

    demographic api

--- app/controllers/api/v2/users_controller.rb
+++ app/controllers/api/v2/users_controller.rb
@@ -5,15 +5,14 @@ module Api::V2
         swagger_controller :users, 'user'
 
         swagger_api :demographic_count do |api|
-            summary 'list followers group by city'
+            summary 'Count of followers'
         end
         def demographic_count
             city_counts = User.followers_by_city_v2(current_user)
             age_counts = User.followers_age_count_v2(current_user)
             ageBetween = []
-            age1421Count = 0
-            age2234Count = 0
-            a = b = {}
+            age1421Count = age2234Count = age3549Count = age5065Count = 0
+            a = b = c = d = {}
             for age_count in age_counts do
                 if age_count.age >= 14 and age_count.age <= 23
                     age1421Count = age1421Count + age_count.count
@@ -29,9 +28,25 @@ module Api::V2
                         count: age2234Count
                     }
                 end
+                if age_count.age >= 35 and age_count.age <= 49
+                    age3549Count = age3549Count + age_count.count
+                    c = {
+                        age: '35 - 49',
+                        count: age3549Count
+                    }
+                end
+                if age_count.age >= 50
+                    age5065Count = age5065Count + age_count.count
+                    d = {
+                        age: '35 - 49',
+                        count: age5065Count
+                    }
+                end
             end
             ageBetween.append(a)
             ageBetween.append(b)
+            ageBetween.append(c)
+            ageBetween.append(d)
             render_success( cities: city_counts, age_counts: ageBetween )
         end
     end
